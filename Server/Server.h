7438c353ef441ef971730a9ae1671cd7142978cc
Jakub Kordel <jakub.kordel@gmail.com>
2021-06-11 22:18:27 +0200

    timeout on server

--- Server/Server.h
+++ Server/Server.h
@@ -7,6 +7,8 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <vector>
+#include <map>
+#include <utility>
 
 #include "Structs.h"
 
@@ -14,6 +16,7 @@ class Server{
 
   std::vector<struct Msg> unhandledRequests;
   std::vector<struct Tuple> tuplesList;
+  std::map<key_t,std::pair<int, int>> mapClients;
 
   key_t mainqueue_key;
   int msgid;
@@ -54,11 +57,34 @@ private:
         std::cout << "Pushing back tuple" << std::endl;
         tuplesList.push_back(msg.req.tuple);
         handleNewTuple(msg.req.tuple);
+    } else
+    if (msg.option == 3) {
+      auto it = mapClients.find(msg.key);
+      if (it->second.first == it->second.second){
+          deleteRequest(msg.key);
+          setHandledRequest(msg.key);
+          sendTimeout(msg.key);
+      }
+      ++(it->second.second);
+
     }
     else {
-        handleRequest(msg);
+      auto it = mapClients.find(msg.key);
+
+      if (it == mapClients.end()){
+        mapClients.insert(std::pair<key_t,std::pair<int,int>>(msg.key, std::pair<int, int>(0,0 ) ));
+      }
+      handleRequest(msg);
     }
   }
+
+  void setHandledRequest(key_t client_key){
+    auto it = mapClients.find(client_key);
+    if (it != mapClients.end()){
+      ++(it->second.first);
+    }
+  }
+
 
   void handleNewTuple(struct Tuple tuple){
         for (auto i = unhandledRequests.begin(); i != unhandledRequests.end() ; ++i){
@@ -66,6 +92,7 @@ private:
               std::cout << "Returning tuple" << std::endl;
               returnTuple(i->key, tuple);
               unhandledRequests.erase(i);
+              setHandledRequest(i->key);
               if (i->option == 1){ //option input
                 tuplesList.pop_back();
                 return;
@@ -80,6 +107,7 @@ private:
         if (compareTupleWithPattern(*i, msg.req.pattern)){
           std::cout << "Returning tuple" << std::endl;
           returnTuple(msg.key, *i);
+          setHandledRequest(msg.key);
           if (msg.option == 1) {
             tuplesList.erase(i);
           }
@@ -205,6 +233,25 @@ private:
     msgbuf.mtype = 1;
     msgbuf.tuple = tuple;
     std::cout << "sending msg to client gid: " << client_msgid << std::endl;
+    msgsnd(client_msgid, &msgbuf, sizeof(struct msgbufTuple), 0);
+  }
+
+  void deleteRequest(key_t client_key){
+    for (auto i = unhandledRequests.begin(); i != unhandledRequests.end() ; ++i){
+      if (i->key == client_key){
+        unhandledRequests.erase(i);
+      }
+    }
+  }
+
+  void sendTimeout(key_t client_key){
+    struct Tuple tuple;
+    tuple.tel_amount = -1;
+    int client_msgid = msgget(client_key, 0); //get users input queue
+    msgbufTuple msgbuf;
+    msgbuf.mtype = 1;
+    msgbuf.tuple = tuple;
+    std::cout << "sending timeout to client gid: " << client_msgid << std::endl;
     msgsnd(client_msgid, &msgbuf, sizeof(struct msgbufTuple), 0);
   }
 };
